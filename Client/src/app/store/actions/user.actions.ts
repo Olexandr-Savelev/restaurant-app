7ddd978696b6a2cfd326f06cc4d42e68ded661f3
Oleksandr Savelev <a.savelev.krm@gmail.com>
2024-03-17 13:58:12 +0200

    add user reducer

--- Client/src/app/store/actions/user.actions.ts
+++ Client/src/app/store/actions/user.actions.ts
@@ -1,5 +1,5 @@
 import { createAction, props } from '@ngrx/store';
-import { User } from 'src/app/models/user.model'; // Make sure to import your User model
+import { User, UserLoginData } from 'src/app/models/user.model'; // Make sure to import your User model
 
 export const loadUser = createAction('[User] Load User');
 
@@ -13,4 +13,7 @@ export const loadUserFailure = createAction(
   props<{ message: string }>()
 );
 
-export const setUser = createAction('[User] Set User', props<{ user: User }>());
+export const setUser = createAction(
+  '[User] Set User',
+  props<{ userData: UserLoginData }>()
+);
--- Client/src/app/store/effects/user.effects.ts
+++ Client/src/app/store/effects/user.effects.ts
@@ -2,8 +2,12 @@ import { Actions, ofType, createEffect } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { catchError, map, mergeMap } from 'rxjs/operators';
 
-import { loadUser, loadUserFailure, setUser } from '../actions/user.actions';
-import { User } from 'src/app/models/user.model';
+import {
+  loadUser,
+  loadUserFailure,
+  loadUserSuccess,
+  setUser,
+} from '../actions/user.actions';
 import { UserService } from 'src/app/shared/services/user.service';
 
 export class UserEffects {
@@ -14,7 +18,19 @@ export class UserEffects {
       ofType(loadUser),
       mergeMap(() =>
         this.userService.getUser().pipe(
-          map((user: User) => setUser({ user })),
+          map((user) => loadUserSuccess({ user })),
+          catchError((error) => of(loadUserFailure({ message: error.message })))
+        )
+      )
+    )
+  );
+
+  setUser$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(setUser),
+      mergeMap((action) =>
+        this.userService.setUser(action.userData).pipe(
+          map((user) => loadUserSuccess({ user })),
           catchError((error) => of(loadUserFailure({ message: error.message })))
         )
       )
